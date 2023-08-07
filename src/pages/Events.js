25b9ed6dc0c4dde9f2e9a6511471c426a0bdcbab
Darren Sexton <dsexton23@icloud.com>
2023-08-07 14:14:49 -0700

    events calanger added

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
-export default function Events() {
-  return (
-    <div>Events</div>
-  )
+
+
+function Events() {
+  const events = [
+    {
+      title: "The Event",
+      start: "2023-09-09T08:00:00",
+      end: "2023-09-10T08:00:00"
+    },
+  ]
+
+    return <div>
+        
+        <Fullcalendar
+        plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
+        initialView={'dayGridMonth'}
+        headerToolbar={
+            {
+                start: "title",
+                center: "",
+                end: "today prev,next"
+            }}
+          height={"90vh"}
+          events={events}
+        />
+    
+    </div>
 }
+
+export default Events;
