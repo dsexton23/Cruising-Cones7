54786c92be7548c8717a968843d65b81985e019c
Darren Sexton <dsexton23@icloud.com>
2023-11-28 11:56:12 -0800

    event fix 11-28

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import "../styles/Footer.css";
 
