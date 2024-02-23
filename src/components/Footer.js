897a31617b680bfe69c3c4b591bdae4e43367bbb
Darren Sexton <dsexton23@icloud.com>
2024-02-22 16:00:17 -0800

    footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,23 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import "../styles/Footer.css";
 
+
 function Footer() {
+  const instagramUrl = 'https://www.instagram.com/cruisingcones7/';
+  const facebookUrl = 'https://www.facebook.com/people/Cruising-Cones-7/';
+
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon /> <FacebookIcon /> 
+
+      <a href={instagramUrl} target="_blank" rel="noopener noreferrer">
+          <InstagramIcon />
+        </a>
+        <a href={facebookUrl} target="_blank" rel="noopener noreferrer">
+          <FacebookIcon />
+        </a>
       </div>
+
       <p> cruisingcones7@gmail.com </p>
     </div>
   );
